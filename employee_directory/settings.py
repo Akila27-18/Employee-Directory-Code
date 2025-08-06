e284baa47f1909fbdbd0ef1339e1ee737c1e2ee9
Akila <akila271819@gmail.com>
2025-08-06 16:09:49 +0530

    initial commit

--- employee_directory/settings.py
+++ employee_directory/settings.py
@@ -95,3 +95,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # --- Login / Logout ---
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/accounts/login/'
+LOGOUT_REDIRECT_URL = '/accounts/logout/'
+
